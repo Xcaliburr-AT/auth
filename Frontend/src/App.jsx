b6f58744ac9e867d0b3685ce1f72caea0b5181f1
Xcaliburr-AT <2020120085@psu.palawan.edu.ph>
2024-11-05 11:05:43 +0800

    Recreating the whole app starting form auth

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import LoginForm from './components/login_components/LoginForm';
+import SignupForm from './components/login_components/Signupform';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const toggleForm = () => {
+      setShowSignup(prev => !prev);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+          {showSignup ? (
+              <SignupForm onShowSignup={toggleForm}/>
+          ) : (
+              <LoginForm onShowSignup={toggleForm} />
+          )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+
+
+
